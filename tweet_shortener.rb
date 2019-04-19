4a165b1d6c478e53a1cb976884333cb86ad46883
Will Jones <willjones2010@gmail.com>
2019-04-19 20:50:39 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,5 @@
 def dictionary
-  words_to_be_substituted = {"hello" => "hi", "to" => 2, "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
+  words_to_be_substituted = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "four" => "4", "for" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
 end
 
 
@@ -8,25 +8,31 @@ def word_substituter(tweet)
   if dictionary.keys.include?(word.downcase)
     word = dictionary[word.downcase]
   else
-    word 
-   end
-  end.join(" ")
+    word
+  end
+end.join(" ")
 end
 
+
 def bulk_tweet_shortener(array)
-  array.each do |item|
-    puts word_substituter(item)
-  end
+  array.each do |tweet| 
+    puts word_substituter(tweet)
+end
 end
 
 def selective_tweet_shortener(tweet)
-  tweet.length > 140 ? word_substituter(tweet) : tweet
-  end
+  if tweet.length > 140  
+    word_substituter(tweet)
+  else 
+    tweet 
+  end 
+end 
+
 
 def shortened_tweet_truncator(tweet)
-   if word_substituter(tweet).chars.length > 140  
-    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
+  if word_substituter(tweet).chars.length > 140
+      truncated = word_substituter(tweet).chars[0..136].push("...").join("")
   else 
-    word_substituter(tweet) 
+    word_substituter(tweet)  
   end 
 end 
